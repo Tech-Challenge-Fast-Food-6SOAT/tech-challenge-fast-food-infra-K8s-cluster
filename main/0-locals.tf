00f5961f7b2a33ce67ec9f7d640d4a75485baebe
Danilo Cassola <danilocassola@gmail.com>
2024-11-16 19:11:44 -0300

    chore: update namespaces

--- main/0-locals.tf
+++ main/0-locals.tf
@@ -1,9 +1,11 @@
 locals {
-  env         = "prod"
-  region      = "us-east-1"
-  zone1       = "us-east-1a"
-  zone2       = "us-east-1b"
-  eks_name    = "lanchonete-cluster"
-  eks_version = "1.30"
-  namespace   = "lanchonete"
+  env                 = "prod"
+  region              = "us-east-1"
+  zone1               = "us-east-1a"
+  zone2               = "us-east-1b"
+  eks_name            = "lanchonete-cluster"
+  eks_version         = "1.31"
+  namespace_pedido    = "lanchonete-pedido"
+  namespace_produto   = "lanchonete-produto"
+  namespace_pagamento = "lanchonete-pagamento"
 }
